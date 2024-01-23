fb35c4b3a6fa05275528be9dc8d193f5699a79e0
tamani phiri <tamanigabriel0@gmail.com>
2024-01-23 20:01:08 +0200

    c

--- src/components/global/Loading.tsx
+++ src/components/global/Loading.tsx
@@ -4,15 +4,24 @@ const Loading = () => {
       key={i}
       className="flex flex-col duration-300 w-full shadow-xl hover:shadow-2xl border overflow-hidden border-[#4c426f] rounded-2xl"
     >
-      <div className=" h-96 w-full bg-[#303030] bg-opacity-30 animate-ping"></div>
+      <div className=" h-96 w-full bg-[#303030] bg-opacity-30 animate-pulse"></div>
       <div className="p-3">
         <div className=" h-3 w-full bg-yellow-300"></div>
         <div className="h-3 w-full bg-red-400"></div>
-        <div className=" h-4 wf rounded-full bg-red-600"></div>
+        <div className=" h-4 wf rounded-full bg-red-600">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Et id
+          nesciunt eligendi saepe natus suscipit? Harum eos error recusandae
+          assumenda porro delectus alias eligendi, ratione minima, ut debitis?
+          Eaque, dolorum?
+        </div>
       </div>
     </div>
   ));
-  return <div className="gap-8 lg:gap-12 py-8 grid md:grid-cols-3 lg:grid-cols-4 w-full">{skeletonItems}</div>;
+  return (
+    <div className="gap-8 lg:gap-12 py-8 grid md:grid-cols-3 lg:grid-cols-4 w-full py-16 min-h-screen px-4 md:px-12">
+      {skeletonItems}
+    </div>
+  );
 };
 
 export default Loading;
