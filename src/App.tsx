58a73b0a930bb7b34761302c2a6397a4256f0c03
tamani phiri <tamanigabriel0@gmail.com>
2024-01-23 15:34:55 +0200

    c

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    axios
+      .get("https://api.themoviedb.org/3/movie/popular",{
+        params:{
+          api_key:"d0f5f2e135336200362af8a1a73acb17"
+        }
+      })
+      .then((res) => console.log(res.data));
+  }, []);
 
   return (
     <>
@@ -29,7 +40,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
