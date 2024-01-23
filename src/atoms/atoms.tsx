32199c61df9d7a01c245ffb308e7fff71c63f267
tamani phiri <tamanigabriel0@gmail.com>
2024-01-23 19:07:55 +0200

    c

--- src/atoms/atoms.tsx
+++ src/atoms/atoms.tsx
@@ -1,5 +1,7 @@
 import { atom } from "recoil";
 import { IMovie } from "../types/interface";
+import { recoilPersist } from "recoil-persist";
+const { persistAtom } = recoilPersist();
 
 export const movieAtomState = atom<IMovie[]>({
   key: "movieAtom",
@@ -9,4 +11,6 @@ export const movieAtomState = atom<IMovie[]>({
 export const likedMoviesState = atom<IMovie[]>({
   key: "likedMovies",
   default: [],
+  dangerouslyAllowMutability: true,
+  effects_UNSTABLE: [persistAtom],
 });
