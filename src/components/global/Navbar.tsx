09fe5688ac262c7b674920501b6a8e2935dd5c0f
tamani phiri <tamanigabriel0@gmail.com>
2024-01-23 16:20:14 +0200

    c

--- src/components/global/Navbar.tsx
+++ src/components/global/Navbar.tsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <nav className=" w-full items-center flex justify-between">
-      nav
+      <span>logo</span>
+      <Link to={"/liked"}>Favorites</Link>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
