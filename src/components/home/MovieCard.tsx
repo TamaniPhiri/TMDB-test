61844de42b90dc4732c2f035e74324d5922757bf
tamani phiri <tamanigabriel0@gmail.com>
2024-01-23 17:48:34 +0200

    c

--- src/components/home/MovieCard.tsx
+++ src/components/home/MovieCard.tsx
@@ -1,5 +1,6 @@
 import { IMG_BASE_URL } from "../../constants/api";
 import { IMovie } from "../../types/interface";
+import IconHeart from "../icons/IconHeart";
 
 interface MovieCardProps {
   movie: IMovie;
@@ -8,12 +9,13 @@ interface MovieCardProps {
 const MovieCard: React.FC<MovieCardProps> = ({ movie }) => {
   return (
     <div className="flex flex-col hover:scale-[101%] duration-300 w-full border overflow-hidden border-gray-700 rounded-2xl">
-      <div>
+      <div className="relative h-96">
+        <IconHeart/>
         <img
           src={IMG_BASE_URL + movie.poster_path}
           alt="movieImg"
           loading="lazy"
-          className=" object-cover w-full h-96 aspect-auto"
+          className=" object-cover w-full h-full aspect-auto"
         />
       </div>
       <div className="p-4">
