4348dc01496b62dfd5406d9d61dc09db184fca53
tamani phiri <tamanigabriel0@gmail.com>
2024-01-23 15:49:18 +0200

    c

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import Liked from "./pages/Liked";
 
 function App() {
   return (
-    <></>
+    <Routes>
+      <Route path="/" Component={Home} />
+      <Route path="/liked" Component={Liked} />
+    </Routes>
   );
 }
 
