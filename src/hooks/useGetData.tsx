43134bc079335117a603bbf925ba0be341ca468e
tamani phiri <tamanigabriel0@gmail.com>
2024-01-23 20:25:58 +0200

    c

--- src/hooks/useGetData.tsx
+++ src/hooks/useGetData.tsx
@@ -4,18 +4,15 @@ import { API_KEY, API_URL } from "../constants/api";
 
 interface GetDataProps {
   endpoint: string;
-  page?: number;
 }
 
 export const UseGet = ({
   endpoint,
-  page,
 }: GetDataProps): (() => Promise<AxiosResponse | any>) => {
   return async () => {
     const res = await axios.get(API_URL + endpoint, {
       params: {
         api_key: API_KEY,
-        page: page,
       },
     });
     return res.data;
