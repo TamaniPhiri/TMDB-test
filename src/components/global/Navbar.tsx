dfb0b6551b8805d43fc2315fb6ee86b77d757fb2
tamani phiri <tamanigabriel0@gmail.com>
2024-01-23 18:41:40 +0200

    c

--- src/components/global/Navbar.tsx
+++ src/components/global/Navbar.tsx
@@ -1,13 +1,21 @@
 import { Link } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+import { likedMoviesState } from "../../atoms/atoms";
 
 const Navbar = () => {
+  const LikedMovies = useRecoilValue(likedMoviesState);
   return (
     <nav style={{ backdropFilter: "blur(10px)" }} className={styles.nav}>
       <Link to={"/"} className={styles.links}>
         Home
       </Link>
       <Link to={"/liked"} className={styles.links}>
-        Favorites
+        <span>Favorites</span>
+        {LikedMovies.length <= 0 ? null : (
+          <div className=" absolute -top-2 -right-2 text-xs rounded-full px-1 bg-red-600">
+            {LikedMovies.length}
+          </div>
+        )}
       </Link>
     </nav>
   );
@@ -16,6 +24,6 @@ const Navbar = () => {
 export default Navbar;
 
 const styles = {
-  links: "hover:text-[#BFCDE0] duration-300",
+  links: "hover:text-[#BFCDE0] duration-300 relative",
   nav: "w-full items-center flex justify-between py-6 px-4 md:px-12 z-50 shadow sticky top-0 bg-[#2c2c2c] bg-opacity-80",
 };
