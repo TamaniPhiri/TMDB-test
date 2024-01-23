aefe638b29487cf0fc05dcbe50e84329d85705b2
tamani phiri <tamanigabriel0@gmail.com>
2024-01-23 17:17:00 +0200

    c

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -26,7 +26,7 @@ const Home = () => {
     return <ErrorDisplay />;
   }
   return (
-    <section className="min-h-screen gap-4 grid md:grid-cols-3 lg:grid-cols-4 w-full">
+    <section className="min-h-screen gap-4 md:gap-8 lg:gap-12 grid md:grid-cols-3 lg:grid-cols-4 w-full">
       {movies.map((movie, i) => (
         <MovieCard key={i} movie={movie} />
       ))}
