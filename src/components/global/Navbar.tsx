385f0448918522f2b9e78e4f79f22ac264556494
tamani phiri <tamanigabriel0@gmail.com>
2024-01-23 16:28:36 +0200

    c

--- src/components/global/Navbar.tsx
+++ src/components/global/Navbar.tsx
@@ -2,11 +2,17 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className=" w-full items-center flex justify-between">
-      <Link to={"/"}>Home</Link>
-      <Link to={"/liked"}>Favorites</Link>
+    <nav className=" w-full items-center flex justify-between py-4 shadow">
+      <Link to={"/"} className={linkStyle}>
+        Home
+      </Link>
+      <Link to={"/liked"} className={linkStyle}>
+        Favorites
+      </Link>
     </nav>
   );
 };
 
 export default Navbar;
+
+const linkStyle = "hover:text-[#FF7F11]";
