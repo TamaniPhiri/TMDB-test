aa491e8f53ed35493ea69f337d67d7d9aa0b6efd
tamani phiri <tamanigabriel0@gmail.com>
2024-01-23 20:34:00 +0200

    c

--- src/pages/Liked.tsx
+++ src/pages/Liked.tsx
@@ -1,15 +1,18 @@
-import { useRecoilValue } from "recoil"
-import { likedMoviesState } from "../atoms/atoms"
+import { useRecoilValue } from "recoil";
+import { likedMoviesState } from "../atoms/atoms";
+import MovieCard from "../components/home/MovieCard";
 
 const Liked = () => {
-  const Liked=useRecoilValue(likedMoviesState);
+  const Liked = useRecoilValue(likedMoviesState);
   console.log(Liked);
-  
+
   return (
     <div>
-      liked
+      {Liked.map((movie, i) => (
+        <MovieCard key={i} movie={movie} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Liked
+export default Liked;
