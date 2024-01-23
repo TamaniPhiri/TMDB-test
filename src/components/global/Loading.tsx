441b087153cfbe09fb8c5e4688d3fd86711262a4
tamani phiri <tamanigabriel0@gmail.com>
2024-01-23 20:20:14 +0200

    c

--- src/components/global/Loading.tsx
+++ src/components/global/Loading.tsx
@@ -2,9 +2,9 @@ const Loading = () => {
   const skeletonItems = Array.from({ length: 8 }, (_, i) => (
     <div key={i} className={styles.card}>
       <div className={styles.imgSkeleton}></div>
-      <div className="p-3 flex flex-col gap-2">
-        <div className=" h-3 w-1/4 bg-[#404040] rounded-full animate-pulse"></div>
-        <div className="h-3 w-full bg-[#404040] rounded-full animate-pulse"></div>
+      <div className={styles.metaSkeleton}>
+        <div className={styles.heading}></div>
+        <div className={styles.date}></div>
         <div className=" h-3 bg-[#404040] w-1/2 text-transparent rounded-full animate-pulse">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Et id
           nesciunt eligendi saepe natus suscipit? Harum eos error
@@ -26,10 +26,12 @@ export default Loading;
 
 const styles = {
   card: "flex flex-col duration-300 w-full shadow-xl hover:shadow-2xl border overflow-hidden border-[#4c426f] rounded-2xl",
-  heading: " text-center text-4xl font-bold",
+  heading: " h-3 w-1/4 bg-[#404040] rounded-full animate-pulse",
   cardContainer:
     "gap-8 lg:gap-12 py-8 grid md:grid-cols-3 lg:grid-cols-4 w-full",
   skeletonContainer:
     "gap-8 lg:gap-12 grid md:grid-cols-3 py-8 lg:grid-cols-4 w-full min-h-screen px-4 md:px-12",
   imgSkeleton: " h-96 w-full bg-[#404040] animate-pulse",
+  metaSkeleton: "p-3 flex flex-col gap-2",
+  date: "h-3 w-full bg-[#404040] rounded-full animate-pulse",
 };
