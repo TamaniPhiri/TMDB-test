525f60b69badfc1a1321a905e1d1279a1ce76500
tamani phiri <tamanigabriel0@gmail.com>
2024-01-23 16:05:40 +0200

    c

--- src/hooks/useGetData.tsx
+++ src/hooks/useGetData.tsx
@@ -5,11 +5,13 @@ interface GetDataProps {
   endpoint: string;
 }
 
-export const UseGet = async ({ endpoint }: GetDataProps) => {
-  const res = await axios.get(API_URL + endpoint, {
-    params: {
-      api_key: API_KEY,
-    },
-  });
-  return res.data;
+export const UseGet = ({ endpoint }: GetDataProps): (() => Promise<void>) => {
+  return async () => {
+    const res = await axios.get(API_URL + endpoint, {
+      params: {
+        api_key: API_KEY,
+      },
+    });
+    return res.data;
+  };
 };
