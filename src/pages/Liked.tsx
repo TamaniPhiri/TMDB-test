7f498bb4e0295f946ed8396e873a7eeec88413c1
tamani phiri <tamanigabriel0@gmail.com>
2024-01-23 20:46:03 +0200

    c

--- src/pages/Liked.tsx
+++ src/pages/Liked.tsx
@@ -6,7 +6,7 @@ const Liked = () => {
   const Liked = useRecoilValue(likedMoviesState);
   return (
     <section className="min-h-screen items-center justify-center flex w-full px-4 md:px-12">
-      {Liked.length >= 0 ? (
+      {Liked.length > 0 ? (
         <main className="gap-8 lg:gap-12 py-16 grid md:grid-cols-3 lg:grid-cols-4 w-full">
           {Liked.map((movie, i) => (
             <MovieCard key={i} movie={movie} />
