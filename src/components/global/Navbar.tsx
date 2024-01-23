6742a7535409f6f778765d85696b0c72832b6a0b
tamani phiri <tamanigabriel0@gmail.com>
2024-01-23 20:13:10 +0200

    c

--- src/components/global/Navbar.tsx
+++ src/components/global/Navbar.tsx
@@ -12,9 +12,7 @@ const Navbar = () => {
       <Link to={"/liked"} className={styles.links}>
         <span>Favorites</span>
         {LikedMovies.length <= 0 ? null : (
-          <div className=" absolute -top-2 -right-2 text-xs rounded-full px-1 bg-red-600">
-            {LikedMovies.length}
-          </div>
+          <div className={styles.badge}>{LikedMovies.length}</div>
         )}
       </Link>
     </nav>
@@ -26,4 +24,5 @@ export default Navbar;
 const styles = {
   links: "hover:text-[#BFCDE0] duration-300 relative",
   nav: "w-full items-center flex justify-between py-6 px-4 md:px-12 z-50 shadow sticky top-0 bg-[#2c2c2c] bg-opacity-80",
+  badge: "absolute -top-2 -right-2 text-xs rounded-full px-1 bg-red-600",
 };
