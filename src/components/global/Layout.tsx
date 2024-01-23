c4c323371395f9ae0cb8dc69373e3d7fa7dec736
tamani phiri <tamanigabriel0@gmail.com>
2024-01-23 16:17:52 +0200

    c

--- src/components/global/Layout.tsx
+++ src/components/global/Layout.tsx
@@ -5,7 +5,7 @@ interface LayoutProps {
 }
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="w-full items-center justify-center px-4 md:px-12 max-w-7xl flex flex-col">
+    <div className="w-full items-center justify-center px-4 md:px-12 max-w-7xl bg-yellow-200 mx-auto flex flex-col">
       <Navbar/>
       <main>{children}</main>
     </div>
