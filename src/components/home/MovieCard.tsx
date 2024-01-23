97474b3b0de3be722fed78bf1671e5f992975460
tamani phiri <tamanigabriel0@gmail.com>
2024-01-23 18:16:44 +0200

    c

--- src/components/home/MovieCard.tsx
+++ src/components/home/MovieCard.tsx
@@ -2,12 +2,27 @@ import moment from "moment";
 import { IMG_BASE_URL } from "../../constants/api";
 import { IMovie } from "../../types/interface";
 import IconHeart from "../icons/IconHeart";
+import { useSetRecoilState } from "recoil";
+import { likedMoviesState } from "../../atoms/atoms";
 
 interface MovieCardProps {
   movie: IMovie;
 }
 
 const MovieCard: React.FC<MovieCardProps> = ({ movie }) => {
+  const likedMovie = useSetRecoilState(likedMoviesState);
+
+  const handleLike = (movies: IMovie) => {
+    likedMovie((prev) => {
+      const isLiked = prev.some((m) => m.id === movies.id);
+
+      if (isLiked) {
+        return prev.filter((m) => m.id !== movies.id);
+      } else {
+        return [...prev, movies];
+      }
+    });
+  };
   return (
     <div className="flex flex-col hover:scale-[101%] duration-300 w-full border overflow-hidden border-gray-700 rounded-2xl">
       <div className="relative h-96">
@@ -21,7 +36,9 @@ const MovieCard: React.FC<MovieCardProps> = ({ movie }) => {
       </div>
       <div className="p-3">
         <h1 className=" font-bold">{movie.title}</h1>
-        <p className="text-[#BFCDE0]">{moment(movie.release_date).format("ll")}</p>
+        <p className="text-[#BFCDE0]">
+          {moment(movie.release_date).format("ll")}
+        </p>
         <p className="">{movie.original_language.toUpperCase()}</p>
       </div>
     </div>
