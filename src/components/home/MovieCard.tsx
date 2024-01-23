4ab46e381d58a10af96263ee3a89b21312182fb2
tamani phiri <tamanigabriel0@gmail.com>
2024-01-23 21:16:13 +0200

    c

--- src/components/home/MovieCard.tsx
+++ src/components/home/MovieCard.tsx
@@ -2,14 +2,16 @@ import moment from "moment";
 import { IMG_BASE_URL } from "../../constants/api";
 import { IMovie } from "../../types/interface";
 import IconHeart from "../icons/IconHeart";
+import { useState } from "react";
 
 interface MovieCardProps {
   movie: IMovie;
 }
 
 const MovieCard: React.FC<MovieCardProps> = ({ movie }) => {
+  const [openOverview, setOpenOverview] = useState<boolean>(false);
   return (
-    <div className="flex flex-col hover:scale-[101%] relative duration-300 w-full shadow-xl hover:shadow-2xl border overflow-hidden border-[#4c426f] rounded-2xl">
+    <div className="flex flex-col hover:scale-[101%] group relative duration-300 w-full shadow-xl hover:shadow-2xl border overflow-hidden border-[#4c426f] rounded-2xl">
       <div className="relative h-96">
         <IconHeart movie={movie} />
         <img
@@ -18,9 +20,20 @@ const MovieCard: React.FC<MovieCardProps> = ({ movie }) => {
           loading="lazy"
           className=" object-cover w-full h-full aspect-auto"
         />
-        <div className="absolute text-sm p-3 flex-1 bg-[#2c2c2c] bottom-0">
+        <div
+          className={`absolute text-sm p-3 flex-1 bg-[#2c2c2c] duration-300 top-0 ${
+            openOverview ? "translate-y-0" : " translate-y-[500px]"
+          }`}
+        >
           {movie.overview}
         </div>
+        <button
+          onClick={() => setOpenOverview(!openOverview)}
+          className=" absolute bottom-3 right-3 group-hover:translate-x-0 translate-x-[500px] duration-300"
+        >
+          {" "}
+          hello
+        </button>
       </div>
 
       <div className="p-3 flex-1">
