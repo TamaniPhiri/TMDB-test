7a23e24c88e980c999a0579180d56551913c767b
tamani phiri <tamanigabriel0@gmail.com>
2024-01-23 20:32:41 +0200

    c

--- src/components/global/Navbar.tsx
+++ src/components/global/Navbar.tsx
@@ -22,7 +22,7 @@ const Navbar = () => {
 export default Navbar;
 
 const styles = {
-  links: "hover:text-[#BFCDE0] duration-300 relative",
+  links: "hover:text-[#BFCDE0] duration-300 relative font-bold",
   nav: "w-full items-center flex justify-between py-6 px-4 md:px-12 z-50 shadow sticky top-0 bg-[#2c2c2c] bg-opacity-80",
   badge: "absolute -top-2 -right-2 text-xs rounded-full px-1 bg-red-600",
 };
