dba197f926d270a4d78f5c6e9e05c3274ecf34a0
tamani phiri <tamanigabriel0@gmail.com>
2024-01-23 15:55:43 +0200

    c

--- src/hooks/useGetData.tsx
+++ src/hooks/useGetData.tsx
@@ -1,11 +1,15 @@
 import axios from "axios";
-import { API_URL } from "../constants/api";
+import { API_KEY, API_URL } from "../constants/api";
 
 interface GetDataProps {
   endpoint: string;
 }
 
 export const UseGet = async ({ endpoint }: GetDataProps) => {
-  const res = await axios.get(API_URL + endpoint);
+  const res = await axios.get(API_URL + endpoint, {
+    params: {
+      api_key: API_KEY,
+    },
+  });
   return res.data;
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <section>
+    <section className=" bg-red-400">
       home
     </section>
   )
