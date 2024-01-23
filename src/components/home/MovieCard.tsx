855e01751d57e1bd22a78d1ee70e8eec7e1962bc
tamani phiri <tamanigabriel0@gmail.com>
2024-01-23 17:40:05 +0200

    c

--- src/components/home/MovieCard.tsx
+++ src/components/home/MovieCard.tsx
@@ -8,12 +8,14 @@ interface MovieCardProps {
 const MovieCard: React.FC<MovieCardProps> = ({ movie }) => {
   return (
     <div className="flex flex-col hover:scale-[101%] duration-300 w-full border overflow-hidden border-gray-700 rounded-2xl">
-      <img
-        src={IMG_BASE_URL + movie.poster_path}
-        alt="movieImg"
-        loading="lazy"
-        className=" object-cover w-full h-96 aspect-auto"
-      />
+      <div>
+        <img
+          src={IMG_BASE_URL + movie.poster_path}
+          alt="movieImg"
+          loading="lazy"
+          className=" object-cover w-full h-96 aspect-auto"
+        />
+      </div>
       <div className="p-4">
         <h1 className=" font-bold">{movie.title}</h1>
       </div>
