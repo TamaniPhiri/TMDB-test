296ced15eaa30c5a9d5979809996f3583d918c4f
tamani phiri <tamanigabriel0@gmail.com>
2024-01-23 20:02:13 +0200

    c

--- src/components/global/Loading.tsx
+++ src/components/global/Loading.tsx
@@ -1,10 +1,10 @@
 const Loading = () => {
-  const skeletonItems = Array.from({ length: 16 }, (_, i) => (
+  const skeletonItems = Array.from({ length: 8 }, (_, i) => (
     <div
       key={i}
       className="flex flex-col duration-300 w-full shadow-xl hover:shadow-2xl border overflow-hidden border-[#4c426f] rounded-2xl"
     >
-      <div className=" h-96 w-full bg-[#303030] bg-opacity-30 animate-pulse"></div>
+      <div className=" h-96 w-full bg-[#181818] bg-opacity-30 animate-pulse"></div>
       <div className="p-3">
         <div className=" h-3 w-full bg-yellow-300"></div>
         <div className="h-3 w-full bg-red-400"></div>
