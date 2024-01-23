abbbcc5075c65b8f2ac62e9c16a7b64fb7bd6041
tamani phiri <tamanigabriel0@gmail.com>
2024-01-23 17:00:57 +0200

    c

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,8 +2,9 @@ import { useQuery } from "react-query";
 import { UseGet } from "../hooks/useGetData";
 import Loading from "../components/global/Loading";
 import ErrorDisplay from "../components/global/ErrorDisplay";
+import { IMovieResponse } from "../types/interface";
 const Home = () => {
-  const { isError, isLoading } = useQuery(
+  const { isError, isLoading } = useQuery<IMovieResponse>(
     "getPopular",
     UseGet({ endpoint: "movie/popular" }),
     {
@@ -20,11 +21,11 @@ const Home = () => {
   if (isError) {
     return <ErrorDisplay />;
   }
-  return <section className=" min-h-[300vh]">home</section>;
+  return <section className={styles.container}>home</section>;
 };
 
 export default Home;
 
-const styles={
-    container:""
-}
+const styles = {
+  container: "min-h-screen",
+};
