b853851d304d66555c1738a91dc01195d6c23ea7
tamani phiri <tamanigabriel0@gmail.com>
2024-01-23 16:49:18 +0200

    c

--- src/types/interface.tsx
+++ src/types/interface.tsx
@@ -1,6 +1,23 @@
 export interface IMovieResponse {
   page: number;
-  results: [];
+  results: IMovie[];
   total_pages: number;
   total_results: number;
+}
+
+export interface IMovie {
+  adult: false;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
 }
