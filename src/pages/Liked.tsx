5b912132ed24800e516c1d6e0acb8fb3c1f13743
tamani phiri <tamanigabriel0@gmail.com>
2024-01-23 20:45:07 +0200

    c

--- src/pages/Liked.tsx
+++ src/pages/Liked.tsx
@@ -5,19 +5,19 @@ import MovieCard from "../components/home/MovieCard";
 const Liked = () => {
   const Liked = useRecoilValue(likedMoviesState);
   return (
-    <div className="min-h-screen items-center justify-center flex w-full px-4 md:px-12">
+    <section className="min-h-screen items-center justify-center flex w-full px-4 md:px-12">
       {Liked.length >= 0 ? (
-        <section className="gap-8 lg:gap-12 py-16 grid md:grid-cols-3 lg:grid-cols-4 w-full">
+        <main className="gap-8 lg:gap-12 py-16 grid md:grid-cols-3 lg:grid-cols-4 w-full">
           {Liked.map((movie, i) => (
             <MovieCard key={i} movie={movie} />
           ))}
-        </section>
+        </main>
       ) : (
         <h1 className="text-center w-full text-2xl font-bold">
           No favorites added
         </h1>
       )}
-    </div>
+    </section>
   );
 };
 
