d85040a2cc699c06bed25db401254d01c9f550ac
tamani phiri <tamanigabriel0@gmail.com>
2024-04-22 03:00:44 +0200

    c

--- src/pages/Liked.tsx
+++ src/pages/Liked.tsx
@@ -25,6 +25,6 @@ const styles = {
   container:
     "min-h-screen items-center justify-center flex w-full px-4 md:px-12",
   cardContainer:
-    "gap-8 lg:gap-12 py-16 grid md:grid-cols-3 lg:grid-cols-4 w-full",
+    "gap-8 lg:gap-12 py-16 grid-col-1 grid md:grid-cols-3 lg:grid-cols-4 w-full",
   header: "text-center w-full text-2xl font-bold",
 };
